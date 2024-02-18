954a8687936ecd2939a57a23f6e40f609568ed27
Pravin Pagare <pravinpgr9@gmail.com>
2024-02-18 12:36:36 +0530

    Updated the codebase from external registration
    Updated the codebase from external registration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -55,13 +55,7 @@
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                </button> 
                             </div>
                         </div>
                     </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,11 +37,7 @@
                                 </li>
                             @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                             
                         @else
                             @canany(['create-role', 'edit-role', 'delete-role'])
                                 <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Roles</a></li>
